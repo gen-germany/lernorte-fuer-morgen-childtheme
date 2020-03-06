64107dcc66d6d5373300aa0a85c8a9dfe0749081
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-03-06 20:07:14 +0100

    Add functions.php

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+function divi__child_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'divi__child_theme_enqueue_styles' );
+ 
+ 
+//you can add custom functions below this line:
